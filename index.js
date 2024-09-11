f2517226c032da9c98c621daade630d8e26bc1cc
12RajYadav <rajyadav29077@gmail.com>
2024-09-11 12:32:34 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import productRoutes from "./routes/products.js";
+import userRoutes from "./routes/users.js";
+import { connectDB } from "./db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Middleware
+app.use(bodyParser.json());
+
+//Using Cors for Specific Origin
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST"],
+  })
+);
+
+//Database Connection
+connectDB();
+
+//Route
+app.use("/products", productRoutes);
+app.use("/user", userRoutes);
+
+//Running server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
